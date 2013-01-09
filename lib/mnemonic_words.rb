35e738d996bf3072e96502f1456144ec1ce52507
Chris Oei <chris@oei.me>
2013-01-08 21:20:52 -0800

    Improve error detection

--- lib/mnemonic_words.rb
+++ lib/mnemonic_words.rb
@@ -1,21 +1,35 @@
 require 'contemporary_words'
 
+# Bitcoin donation address: 1MWeioFdnwFZC8eqJwxjKcA3SErKxEBu8V
+
 class MnemonicWords
 
-  attr_accessor :base, :p_error, :range, :n_digits, :multiplier, :word_list
+  class << self
 
-  def initialize(range, p_error, base = 2357)
+    attr_reader :multipliar
+
+  end
+
+  @multipliar = [ 955, 3798375, 4966887791, 4886399928973, 310731420136098447 ]
+
+  attr_accessor :base, :p_error, :p_error_max, :range, :n_digits, :multiplier, :divisor, :word_list
+
+  def initialize(range, p_error_max, base = 2357)
     @base = base
-    @p_error = p_error
+    @p_error_max = p_error_max
     @range = range
-    @n_digits = (Math.log(@range.size, @base) - Math.log(@p_error, @base)).ceil
-    @multiplier = ((@base ** @n_digits).to_f / @range.size).floor
+    @n_digits = (Math.log(@range.size, @base) - Math.log(@p_error_max, @base)).ceil
+    @max_result = @base ** @n_digits
+    @p_error = @range.size.to_f/@max_result
+    @multiplier = (@n_digits > MnemonicWords.multipliar.size) ?
+      (0.7 * @max_result).floor : MnemonicWords.multipliar[@n_digits - 1]
+    @divisor = inverse(@multiplier, @max_result)
     @word_list = ContemporaryWords.all[0..@base-1]
     true
   end
 
   def encode(x)
-    y = x * @multiplier
+    y = ((x - @range.begin) * @multiplier) % @max_result
     a = []
     while y > 0 do
       a.unshift y % @base
@@ -25,9 +39,38 @@ class MnemonicWords
   end
 
   def decode(s)
-    y = s.split(/\s+/).inject(0) { |memo,x| memo * @base + @word_list.index(x) }
-    return nil unless (y % @multiplier) == 0
-    y / @multiplier
+    indices = s.downcase.split(/\s+/).map { |word| @word_list.index(word) }
+    return nil if indices.any? &:nil?
+    y = indices.inject(0) { |memo,x| memo * @base + x }
+    result = ((y * @divisor) % @max_result) + @range.begin
+    (@range === result) ? result : nil
+  end
+
+  def inspect
+    "< n: #{@n_digits}, -log10(p): #{-Math.log10(@p_error)} >"
+  end
+
+  private
+
+  # Adapted from http://www.dzone.com/snippets/modular-inverse-function
+  def inverse(a, n )
+    i = n
+    v = 0 
+    d = 1
+    while a>0 do
+      t = (i/a).floor
+      x = a
+      a = i % x
+      i = x
+      x = d
+      d = v - t*x
+      v = x
+    end
+    v %= n
+    if (v<0)
+      v = (v+n)%n
+    end
+    return v
   end
 
 end
---
+++ spec/mnemonic_words_spec.rb
@@ -0,0 +1,14 @@
+require 'mnemonic_words'
+
+describe MnemonicWords do
+	
+	it 'should convert a phone number to words' do
+		mw = MnemonicWords.new((0..9999999999), 1)
+		mw.p_error.should be_within(0.02).of(0.76)
+		s = mw.encode(4155551212)
+		s.should == 'clue brought dear'
+		mw.decode(s).should == 4155551212
+		mw.decode('clue brought deer').should be_nil
+	end
+	
+end
